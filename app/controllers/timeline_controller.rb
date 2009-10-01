8809dce667d4574ef4fce39528e3ce234febb7a1
lizconlan <Liz@liz-conlans-macbook.local>
2009-10-01 11:01:34 +0100

    trying to show favorites instead of timeline

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,6 +2,6 @@ class TimelineController < ApplicationController
   before_filter :login_required
   
   def index
-    @tweets = current_user.twitter.get('/statuses/friends_timeline') 
+    @tweets = current_user.twitter.get('/favorites') 
   end
 end
