e819e7ffda25cbb09381a16ba69501fe5b049866
lizconlan <Liz@liz-conlans-macbook.local>
2009-10-01 01:17:45 +0100

    oAuth test files

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,8 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
+  map.connect '/test' :controller => 'timeline', :action => 'index'
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
