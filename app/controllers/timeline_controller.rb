61ef217e70f44a324e896b6fe8c351d4843a3893
lizconlan <Liz@liz-conlans-macbook.local>
2009-10-14 00:12:21 +0100

    adjusted view and controller to work with updated user model

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,12 +2,6 @@ class TimelineController < ApplicationController
   before_filter :login_required
   
   def index
-    current_user.load_favorites
-    page = params[:page]
-    if page
-      @tweets = current_user.twitter.get("/favorites?page=#{page}") 
-    else
-      @tweets = current_user.twitter.get('/favorites') 
-    end
+    @tweets = current_user.tweets
   end
 end
