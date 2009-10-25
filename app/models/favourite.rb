fac42b8244cd125a22c11edd439275bcf18677d8
lizconlan <Liz@liz-conlans-macbook.local>
2009-10-25 18:25:32 +0000

    making usernames and links in tweets clickable; closes gh-18; closes gh-21

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,18 @@
 class Favourite < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :tags
+  
+  def html_text
+    html = text
+    html.scan(/http:\/\/\S*/).each do |match|
+      html.gsub!(match, "<a href='#{match}'>#{match}</a>")
+    end
+    html.scan(/ @\S*/).each do |match|
+      html.gsub!(match, " <a href='http://twitter.com/#{match.gsub(" @", "")}'>#{match.strip}</a>")
+    end
+    html.scan(/^@\S*/).each do |match|
+      html.gsub!(match, " <a href='http://twitter.com/#{match.gsub("@", "")}'>#{match.strip}</a>")
+    end
+    html
+  end
 end
