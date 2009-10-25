c20f1cbfde7411cb3ed36e2a0628293443c890a6
lizconlan <Liz@liz-conlans-macbook.local>
2009-10-25 18:40:07 +0000

    better links to twitter account names

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -7,10 +7,10 @@ class Favourite < ActiveRecord::Base
     html.scan(/http:\/\/\S*/).each do |match|
       html.gsub!(match, "<a href='#{match}'>#{match}</a>")
     end
-    html.scan(/ @\S+/).each do |match|
+    html.scan(/ @[a-zA-Z0-9]+/).each do |match|
       html.gsub!(match, " <a href='http://twitter.com/#{match.gsub(" @", "")}'>#{match.strip}</a>")
     end
-    html.scan(/^@\S+/).each do |match|
+    html.scan(/^@[a-zA-Z0-9]+/).each do |match|
       html.gsub!(match, " <a href='http://twitter.com/#{match.gsub("@", "")}'>#{match.strip}</a>")
     end
     html
