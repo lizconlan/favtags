75ad097140c933fdce6e1a764ac423a76e55e210
lizconlan <Liz@liz-conlans-macbook.local>
2010-03-13 19:25:59 +0000

    few tweaks to favorite model; added spec for favorite model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -18,16 +18,16 @@ class Favorite < ActiveRecord::Base
   def html_text
     html = text
     html.scan(/https*:\/\/\S*/).each do |match|
-      html.gsub!(match, "<a href='#{match}'>#{match}</a>")
+      html.gsub!(match, "<a href=\"#{match}\">#{match}</a>")
     end
     html.scan(/(?:\W|,)(@[a-zA-Z0-9_]+)/).each do |match|
-      html.gsub!(match.to_s, %Q| <a href="http://twitter.com/#{match.first.gsub("@", "")}">#{match.to_s.strip}</a>|)
+      html.gsub!(match.to_s, %Q|<a href="http://twitter.com/#{match.first.gsub("@", "")}">#{match.to_s.strip}</a>|)
     end
     html.scan(/^(@[a-zA-Z0-9_]+)/).each do |match|
-      html.gsub!(match.to_s, " <a href='http://twitter.com/#{match.first.gsub("@", "")}'>#{match.to_s.strip}</a>")
+      html.gsub!(match.to_s, "<a href=\"http://twitter.com/#{match.first.gsub("@", "")}\">#{match.to_s.strip}</a>")
     end
     html.scan(/(?:\ |!|\(|^)(#[a-zA-Z0-9_]+)/).each do |match|
-      html.gsub!(match.to_s, " <a href='http://search.twitter.com/search?q=#{match.first.to_s.gsub('#','%23')}'>#{match.first.to_s.strip}</a>")
+      html.gsub!(match.to_s, "<a href=\"http://search.twitter.com/search?q=#{match.first.to_s.gsub('#','%23')}\">#{match.first.to_s.strip}</a>")
     end
     html.gsub!("\n", "<br />")
     html.to_s
---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,106 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+describe Favorite do
+  
+  describe 'when asked for all favorites by tag_name and user_id' do
+    before do
+      @fave1 = mock_model(Favorite)
+      @fave2 = mock_model(Favorite)
+    end
+    
+    it 'should return an array of favorites' do
+      tag = mock_model(Tag, :favorites => [@fave1, @fave2])
+      Tag.should_receive(:find_by_name_and_user_id).with('test', 1).and_return(tag)
+      
+      Favorite.find_all_by_tag_name_and_user_id('test', 1).should == [@fave1, @fave2]
+    end
+  end
+  
+  describe 'when asked for html_text' do
+    it 'should make urls in the text into links' do
+      fave = Favorite.new(:text => "I like http://twitter.com")
+      fave.html_text.should == 'I like <a href="http://twitter.com">http://twitter.com</a>'
+    end
+    
+    it 'should link user names back to the relevant twitter page' do
+      fave = Favorite.new(:text => "Are you following @spam on Twitter?")
+      fave.html_text.should == 'Are you following <a href="http://twitter.com/spam">@spam</a> on Twitter?'
+      
+      fave = Favorite.new(:text => "@spam is worth following")
+      fave.html_text.should == '<a href="http://twitter.com/spam">@spam</a> is worth following'
+    end
+    
+    it 'should link hashtags to search.twitter.com' do
+      fave = Favorite.new(:text => "wish I'd written these specs sooner #lazy")
+      fave.html_text.should == 'wish I\'d written these specs sooner <a href="http://search.twitter.com/search?q=%23lazy">#lazy</a>'
+    end
+  end
+  
+  describe 'when checking whether it has a particular tag' do
+    before do
+      @fave = Favorite.new()
+      @fave.stub!(:tag_names => ['test', 'test2'])
+    end
+    
+    it 'should return true if the tag is included' do
+      @fave.has_tag?('test').should == true
+    end
+    
+    it 'should return false if the tag is not included' do
+      @fave.has_tag?('test1').should == false
+    end
+  end
+  
+  describe 'when asked for the tag names' do
+    it 'should return a list of tag names' do
+      tag1 = mock_model(Tag, :name => 'test')
+      tag2 = mock_model(Tag, :name => 'test1')
+      fave = Favorite.new(:tags => [tag1, tag2])
+      fave.tag_names.should == ['test', 'test1']
+    end
+  end
+  
+  describe 'when asked to add a tag' do
+    before do
+      tag1 = mock_model(Tag)
+      @fave = Favorite.new(:tags => [tag1])
+    end
+    
+    it 'should not attempt to add the tag if the tag is already included' do
+      @fave.should_receive(:has_tag?).with('test').and_return(true)
+      @fave.tags.should_not_receive(:<<)
+      
+      @fave.tag('test', 1)
+    end
+    
+    it 'should add the tag if the tag is not already included' do
+      tag = mock_model(Tag)
+      @fave.should_receive(:has_tag?).with('test').and_return(false)
+      Tag.should_receive(:find_or_create_by_name_and_user_id).with('test', 1).and_return(tag)
+      @fave.tags.should_receive(:<<).with(tag)
+      
+      @fave.tag('test', 1)
+    end
+  end
+  
+  describe 'when asked to remove a tag' do
+    before do
+      @tag1 = mock_model(Tag)
+      @fave = Favorite.new(:tags => [@tag1])
+    end
+    
+    it 'should do nothing if the tag is not included' do
+      @fave.should_receive(:has_tag?).and_return(false)
+      @fave.tags.should_not_receive(:find_by_name)
+      
+      @fave.detag('test')
+    end
+    
+    it 'should remove the tag if the tag is included' do
+      @fave.should_receive(:has_tag?).and_return(true)
+      @fave.tags.should_receive(:find_by_name).and_return(@tag1)
+      @fave.tags.should_receive(:delete).with(@tag1)
+      
+      @fave.detag('test')
+    end
+  end
+end
