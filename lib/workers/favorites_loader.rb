c2775f6c5a4bf2f8fc2fdcf36a77ff5be9e91527
lizconlan <Liz@liz-conlans-macbook.local>
2010-03-14 20:18:46 +0000

    tidied up background job code

--- lib/workers/favorites_loader.rb
+++ lib/workers/favorites_loader.rb
@@ -3,23 +3,9 @@ require 'models/user.rb'
 require 'models/favorite.rb'
 
 class FavoritesLoader
-  
-  #include BackgroundFu::WorkerMonitoring
-    
+      
   def load_user_favorites user_id
-    #my_progress = 0
-
     current_user = User.find_by_id(user_id.to_i)
-    
-    #record_progress(my_progress)
-
     current_user.load_favorites
-    #while(my_progress < 100)
-    #  my_progress += 1
-    #  record_progress(my_progress)
-    #  sleep 1
-    #end
-    
-    #record_progress(100)
   end
 end
