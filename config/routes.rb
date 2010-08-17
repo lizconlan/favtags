6e92da4c490b90a42cd0923d8f033b16fca08427
lizconlan <lizconlan@gmail.com>
2010-08-17 17:27:12 +0100

    fixed the unfave/delete route

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,17 @@ ActionController::Routing::Routes.draw do |map|
 
   map.favorites '/favorites', :controller => 'favorites', :action => 'index'
   map.connect '/favorites', :controller => 'favorites', :action => 'tag'
-  map.connect '/favorites', :controller => 'favorites', :action => 'delete'
-  map.connect '/favorites/tag', :controller => 'favorites', :action => 'tag'
-  map.connect '/favorites/new_tag', :controller => 'favorites', :action => 'new_tag'
+  map.connect '/favorites/unfave', :controller => 'favorites', :action => 'delete'
+  
   map.connect '/favorites/accounts', :controller => 'favorites', :action => 'accounts'
   map.connect '/favorites/accounts/:account', :controller => 'favorites', :action => 'index'
+  
+  map.connect '/favorites/tag', :controller => 'favorites', :action => 'tag'
   map.connect '/favorites/tags', :controller => 'favorites', :action => 'tags'
   map.connect '/favorites/tags/:tag', :controller => 'favorites', :action => 'index'
   map.connect '/favorites/:id/detag/:tag', :controller => 'favorites', :action => 'remove_tag'
+  map.connect '/favorites/new_tag', :controller => 'favorites', :action => 'new_tag'
+  
   map.connect '/credits', :controller => 'application', :action => 'credits'
   
   map.connect '*bad_route', :controller => 'application', :action => 'render_not_found'
