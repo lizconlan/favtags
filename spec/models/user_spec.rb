bd87159dcde587ac1b0aba4876a6d691735f7878
lizconlan <lizconlan@gmail.com>
2010-08-30 19:56:36 +0100

    basic user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+
 describe User do
   describe 'when asked to delete favorites' do
     it 'should delete all stored favorites for current user' do
