f278969dfe38416f6e56db20eddd7f56a866d260
lizconlan <lizconlan@gmail.com>
2010-08-30 19:12:13 +0100

    hardcoding RAILS_ENV=production for cap tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -134,21 +134,17 @@ end
 
 
 namespace :delayed_job do  
-  def rails_env
-    fetch(:rails_env, false) ? "RAILS_ENV=#{fetch(:rails_env)}" : ''
-  end
-  
   task :stop, :roles => :app do
-    run "cd #{current_path};#{rails_env} script/delayed_job stop"
+    run "cd #{current_path};RAILS_ENV=production script/delayed_job stop"
   end
 
   desc "Start the delayed_job process"
   task :start, :roles => :app do
-    run "cd #{current_path};#{rails_env} script/delayed_job start"
+    run "cd #{current_path};RAILS_ENV=production script/delayed_job start"
   end
 
   desc "Restart the delayed_job process"
   task :restart, :roles => :app do
-    run "cd #{current_path};#{rails_env} script/delayed_job restart"
+    run "cd #{current_path};RAILS_ENV=production script/delayed_job restart"
   end
 end
