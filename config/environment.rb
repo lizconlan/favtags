17239532fe3cc52a534b0c79ad67b52e6470c230
lizconlan <Liz@liz-conlans-macbook.local>
2010-12-31 22:08:34 +0000

    ...best tell it about the gem though

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "will_paginate"
   config.gem "json"
   config.gem "twitter"
+  config.gem "haml"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
